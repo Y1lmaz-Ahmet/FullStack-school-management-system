cc6ff0908401bec8bc55864a9e33864a0eed2305
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-25 21:43:57 +0200

    aanmaken Base classe Mens

---
+++ BusinessLaag/klassen/Mens.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLaag.klassen
+{
+    public class Mens
+    {
+        #region Properties
+        public string Voornaam { get;private set; }
+        public string FamilieNaam { get; private set; }
+        public string Adres { get;private set; }
+        public string Email { get; set; }
+        public int RijksregisterNummer { get;private set; }
+        #endregion
+
+        #region Constructor
+        public Mens(string voornaam, string familieNaam, string adres, string email, int rijksregisterNummer)
+        {
+            Voornaam = voornaam;
+            FamilieNaam = familieNaam;
+            Adres = adres;
+            Email = email;
+            RijksregisterNummer = rijksregisterNummer;
+        }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{this.GetType()},{Voornaam},{FamilieNaam},{Adres},{Email},{RijksregisterNummer}";
+        }
+        #endregion
+    }
+}
