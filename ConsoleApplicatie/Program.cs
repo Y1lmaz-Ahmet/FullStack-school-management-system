521ff7f098da9013e222ba4800d2dcccd02800da
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-26 12:45:24 +0200

    klaar met aanmaken classen en testen in de console!

--- ConsoleApplicatie/Program.cs
+++ ConsoleApplicatie/Program.cs
@@ -12,22 +12,46 @@ namespace ConsoleApplicatie
     {
         static void Main(string[] args)
         {
-            List<string> Vakken = new List<string>() {"Wiskune","Nederlands","Engels","Aardrijkskunde","LichaamlijkeOpvoeding" };
+            int consoleWidth = Console.WindowWidth;
+            string line = new string('*', consoleWidth);
+            Console.WriteLine(line);
+            List<string> Vakken = new List<string>() { "Wiskunde", "Nederlands", "Engels", "Aardrijkskunde", "Lichamelijke Opvoeding" };
+            
             // Maak een leerling aan
-            Leerling leerling = new Leerling("Jan", "Janssen", "Steenweg 1", "jan.janssen@test.be", "01010100101",new DateTime(1999,12,07), 1, "1A");
+            Leerling leerling = new Leerling("Jan", "Janssen", "Steenweg 1", "jan.janssen@test.be", "01010100101", new DateTime(1999, 12, 07), 1, "1A");
 
-            //// Maak een leerkracht aan
-            Leerkracht leerkracht = new Leerkracht("Piet", "Pieters", "Marktstraat 2", "piet.pieters@test.be", "02020200202", new DateTime(1978, 12, 06), Vakken,WerknemerType.Fulltime);
+            Console.WriteLine($"{leerling.Functie}: {leerling.Voornaam} {leerling.FamilieNaam}");
+            Console.WriteLine($"Adres: {leerling.Adres}");
+            Console.WriteLine($"Email: {leerling.Email}");
+            Console.WriteLine($"Rijksregisternummer: {leerling.RijksregisterNummer}");
+            Console.WriteLine($"Geboortedatum: {leerling.GeboorteDatum.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Leerjaar: {leerling.LeerJaar}");
+            Console.WriteLine($"Klaslokaal: {leerling.KlasLokaal}");
+            Console.WriteLine(line);
+            // Maak een leerkracht aan
+            Leerkracht leerkracht = new Leerkracht("Piet", "Pieters", "Marktstraat 2", "piet.pieters@test.be", "02020200202", new DateTime(1978, 12, 06), Vakken, WerknemerType.Fulltime);
 
-            //// Toon de gegevens van de leerling
-            Console.WriteLine($"{leerling.Functie}: {leerling.Voornaam} is ingeschreven in leerjaar {leerling.LeerJaar} en zit in klaslokaal {leerling.KlasLokaal}.");
+            Console.WriteLine($"{leerkracht.Functie}: {leerkracht.Voornaam} {leerkracht.FamilieNaam}");
+            Console.WriteLine($"Adres: {leerkracht.Adres}");
+            Console.WriteLine($"Email: {leerkracht.Email}");
+            Console.WriteLine($"Rijksregisternummer: {leerkracht.RijksregisterNummer}");
+            Console.WriteLine($"Geboortedatum: {leerkracht.GeboorteDatum.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Vakken: {string.Join(", ", leerkracht.Vakken)}");
+            Console.WriteLine($"Werknemer type: {leerkracht.WerknemerType}");
+            Console.WriteLine(line);
 
-            //// Toon de gegevens van de leerkracht
-            Console.WriteLine($"{leerkracht.Functie}: {leerkracht.Voornaam} geeft het vak {string.Join(", ",Vakken)}.");
+            // Maak een administratief medewerker
+            AdministratiefMedewerker admin = new AdministratiefMedewerker("femke", "Den Bosch", "FemkeStraat 16 w2", "Femke@admin.com", "12345678912", new DateTime(1980, 6, 7), WerknemerType.Parttime);
 
-            Console.WriteLine($"{leerkracht.Functie}: {leerkracht.Voornaam} is geboren op:{leerkracht.GeboorteDatum}\n en hij werkt {leerkracht.WerknemerType}");
-            Console.WriteLine($"{leerling.Functie}: {leerling.Voornaam} is geboren op:{leerling.GeboorteDatum}");
+            Console.WriteLine($"{admin.Functie}: {admin.Voornaam} {admin.FamilieNaam}");
+            Console.WriteLine($"Adres: {admin.Adres}");
+            Console.WriteLine($"Email: {admin.Email}");
+            Console.WriteLine($"Rijksregisternummer: {admin.RijksregisterNummer}");
+            Console.WriteLine($"Geboortedatum: {admin.GeboorteDatum.ToString("dd/MM/yyyy")}");
+            Console.WriteLine($"Werknemer type: {admin.WerknemerType}");
+            Console.WriteLine(line);
 
+            //Zodat de console niet sluit.
             Console.ReadLine();
         }
     }
