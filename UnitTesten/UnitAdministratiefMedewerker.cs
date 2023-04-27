3471f1c5e3b773a1c28135c4ff048016e0647193
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-27 16:15:16 +0200

    test afronden.

---
+++ UnitTesten/UnitAdministratiefMedewerker.cs
@@ -0,0 +1,37 @@
+﻿using BusinessLaag.Enums;
+using BusinessLaag.klassen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTesten
+{
+    public class UnitAdministratiefMedewerker
+    {
+        [Fact]
+        public void aanmakenAdministratiefMedewerker()
+        {
+            string voornaam = "John";
+            string familieNaam = "Doe";
+            string adres = "Voorbeeldstraat 1";
+            string email = "john.doe@example.com";
+            string rijksregisterNummer = "12345678923";
+            DateTime geboorteDatum = new DateTime(1998, 12, 1);
+            
+            AdministratiefMedewerker admin = new AdministratiefMedewerker(voornaam,familieNaam,adres,
+                email,rijksregisterNummer,geboorteDatum,WerknemerType.Parttime);
+            Assert.IsType<AdministratiefMedewerker>(admin);
+            Assert.NotNull(admin);
+            Assert.Equal(voornaam,admin.Voornaam);
+            Assert.Equal(familieNaam,admin.FamilieNaam);
+            Assert.Equal(admin.Adres,admin.Adres);  
+            Assert.Equal(email,admin.Email);
+            Assert.Equal(rijksregisterNummer,admin.RijksregisterNummer);
+            Assert.Equal(geboorteDatum,admin.GeboorteDatum);
+
+
+        }
+    }
+}
